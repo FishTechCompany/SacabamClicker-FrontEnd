e970894ce57cd71e5c81e339a101ba5bb28648ed
Phalkun <lehuydiep4@gmail.com>
2025-12-26 00:11:08 +0700

    Update axiosConfig.ts
    Final tweak

--- src/api/config/axiosConfig.ts
+++ src/api/config/axiosConfig.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1';
+const API_BASE_URL = '/api/v1';
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
